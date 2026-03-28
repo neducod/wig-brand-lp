4bbde228f5ac5c4ea1d1e3bf689cb279363f941d
neducod <nedunony16@gmail.com>
2026-03-28 23:43:36 +0100

    adding react icons

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -2,6 +2,8 @@ import React, { useState } from 'react';
 
 import footerImage from './assets/Gemini_Generated_Image_mi91rxmi91rxmi91.png'
 
+import { RiTwitterXFill } from "react-icons/ri";
+
 function FooterSection (){
     return (
         <div className='bg-[#fffef2] flex justify-start flex-col gap-6  p-6'>
@@ -9,7 +11,7 @@ function FooterSection (){
             <div className='flex flex-col'>
                 <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laudantium laborum natus, minus eos nostrum numquam quasi dolor ex fuga!</p>
                 <div className='flex gap-4 py-6'>
-                    <button className='relative inline-flex items-center justify-center gap-2 text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 bg-blue-500 hover:bg-[#F5F5F5] h-9 rounded-md px-3 group'>Explore Shop</button>
+                    <button className='relative inline-flex items-center justify-center gap-2 text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 bg-[#fcaa2d] hover:bg-[#F5F5F5] h-9 rounded-md px-3 group'>Explore Shop</button>
                     <button className='relative inline-flex items-center justify-center gap-2 text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 bg-pink-300 hover:bg-[#F5F5F5] h-9 rounded-md px-3 group'>Watch hair videos</button>
                 </div>
             </div>
@@ -17,14 +19,15 @@ function FooterSection (){
             <div className='flex flex-col gap-4'>
                 <h6 className='text-base hidden'>Wig brand</h6>
                 <ul className='flex flex-row gap-2'>
-                    <li>Email Us</li>
-                    <li>About</li>
-                    <li>Privacy policy</li>
+                    <li className='text-sm'>Email Us</li>
+                    <li className='text-sm'>About</li>
+                    <li className='text-sm'>Privacy policy</li>
                 </ul>
                 {/* TWO ICONS FOR ONLY LARGER SCREENS */}
             </div>
             <hr className='bg-[#ccb796]' />
             <h6 className='text-xl'>Wig brand</h6>
+            <RiTwitterXFill />
             <div className='flex flex-col gap-2'>
                 <h6 className='hidden sm:block'>Copyright @ 2026</h6>
                 <ul className='flex flex-col gap-2'>
