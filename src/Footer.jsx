337a3c54ff5fd2704e5b6c2aea36359bd8b3cc35
neducod <nedunony16@gmail.com>
2026-03-28 23:12:11 +0100

    DOne for the day

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,9 +1,10 @@
 import React, { useState } from 'react';
 
+import footerImage from './assets/Gemini_Generated_Image_mi91rxmi91rxmi91.png'
 
 function FooterSection (){
     return (
-        <div className='flex justify-start  flex-col gap-6  p-6'>
+        <div className='bg-[#fffef2] flex justify-start flex-col gap-6  p-6'>
             <h6 className='text-4xl'>Get ready to style for <span className='text-red-200'>hair</span> </h6>
             <div className='flex flex-col'>
                 <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laudantium laborum natus, minus eos nostrum numquam quasi dolor ex fuga!</p>
@@ -12,17 +13,18 @@ function FooterSection (){
                     <button className='relative inline-flex items-center justify-center gap-2 text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 bg-pink-300 hover:bg-[#F5F5F5] h-9 rounded-md px-3 group'>Watch hair videos</button>
                 </div>
             </div>
-            <img src="" alt="" />
+            <img src={footerImage} alt="" />
             <div className='flex flex-col gap-4'>
-                <h6>Wig brand</h6>
+                <h6 className='text-base hidden'>Wig brand</h6>
                 <ul className='flex flex-row gap-2'>
                     <li>Email Us</li>
                     <li>About</li>
                     <li>Privacy policy</li>
                 </ul>
-                {/* TWO ICONS */}
+                {/* TWO ICONS FOR ONLY LARGER SCREENS */}
             </div>
-            <hr />
+            <hr className='bg-[#ccb796]' />
+            <h6 className='text-xl'>Wig brand</h6>
             <div className='flex flex-col gap-2'>
                 <h6 className='hidden sm:block'>Copyright @ 2026</h6>
                 <ul className='flex flex-col gap-2'>
@@ -30,7 +32,7 @@ function FooterSection (){
                     <li>Status</li>
                     <li>Videos</li>
                 </ul>
-                <h6 className='sm:hidden py-4'>Copyright @ 2026</h6>
+                <h6 className='sm:hidden py-4 text-sm'>Copyright @ 2026</h6>
             </div>
         </div>
     );
